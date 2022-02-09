7737cb28fff687e6bc94d872edf6279546a80e26
Mohamed Tanash <tanashkoo@gmail.com>
2022-02-10 00:27:39 +0200

    enhance model validations and add new field for payment info

--- models/order.js
+++ models/order.js
@@ -5,17 +5,20 @@ const orderSchema = new mongoose.Schema(
     orderOwner: {
       type: mongoose.SchemaTypes.ObjectId,
       ref: "user",
+      required: true,
     },
     orderProducts: [
       {
         type: mongoose.SchemaTypes.ObjectId,
         ref: "product",
+        required: true,
       },
     ],
     orderTotalPrice: Number,
     orderStatus: {
       type: String,
       enum: ["confirmed", "shipped", "arrived"],
+      default: "confirmed",
     },
     orderInfo: {
       firstName: String,
@@ -23,12 +26,18 @@ const orderSchema = new mongoose.Schema(
       address: String,
       phoneNumber: String,
     },
+    paymentMethodInfo: {
+      creditNumber: Number,
+      creditExpMonth: Number,
+      creditExpYear: Number,
+      creditCVC: Number,
+    },
   },
   {
     timestamps: true,
   }
 );
 
-const Order = mongoose.model("order", orderSchema, "order");
+const Order = mongoose.model("order", orderSchema);
 
 module.exports = Order;
