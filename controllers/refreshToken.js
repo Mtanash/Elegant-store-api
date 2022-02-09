1b7919a686a7c7006d7e635bab98c26afd8ddcb7
Mohamed Tanash <tanashkoo@gmail.com>
2022-02-10 00:20:10 +0200

    add getRefreshToken function that take a refresh token to generate a new accessToken

---
+++ controllers/refreshToken.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const getRefreshToken = async (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(401);
+  const refreshToken = cookies.jwt;
+
+  jwt.verify(
+    refreshToken,
+    process.env.JWT_REFRESH_SECRET,
+    async (err, decoded) => {
+      if (err) return res.sendStatus(403);
+      const user = await User.findOne({ _id: decoded._id, refreshToken });
+      if (!user) return res.sendStatus(403);
+      const accessToken = user.generateAccessToken();
+      res.json({ accessToken });
+    }
+  );
+};
+
+module.exports = getRefreshToken;
