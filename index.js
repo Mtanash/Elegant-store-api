1d908f47a24d039638a53d9e7502f339d6c424ec
Mohamed Tanash <tanashkoo@gmail.com>
2022-02-10 00:15:58 +0200

    add cors config to whitelist domains

--- index.js
+++ index.js
@@ -5,14 +5,32 @@ const bodyParser = require("body-parser");
 const userRouter = require("./routes/user");
 const productRouter = require("./routes/product");
 const orderRouter = require("./routes/order");
+const cookieParser = require("cookie-parser");
 require("./config/DB");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(cookieParser());
 app.use(bodyParser.json({ limit: "10mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
-app.use(cors());
+
+const whitelist = [
+  "http://localhost:3000",
+  "https://elegant.mohamedtanash.com",
+];
+const corsOption = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+app.use(cors(corsOption));
 
 app.use("/users", userRouter);
 app.use("/products", productRouter);
