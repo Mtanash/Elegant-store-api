56bb99bdae558c03b817632dcb836ce58cb4edb0
Mohamed Tanash <tanashkoo@gmail.com>
2022-02-21 13:58:43 +0200

    removed title property

--- models/product.js
+++ models/product.js
@@ -2,10 +2,6 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
-    title: {
-      type: String,
-      required: [true, "Please provide product title"],
-    },
     description: {
       type: String,
       required: [true, "Please provide product description"],
