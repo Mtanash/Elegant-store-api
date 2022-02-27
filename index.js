e82ef5766ffead50ea1d78aca0239ecc1042d3d3
Mohamed Tanash <tanashkoo@gmail.com>
2022-02-27 14:52:18 +0200

    refactor db connection function and start server function

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const userRouter = require("./routes/user");
 const productRouter = require("./routes/product");
 const orderRouter = require("./routes/order");
 const dashboardRouter = require("./routes/dashboard");
 const corsOption = require("./config/corsOption");
-const cookieParser = require("cookie-parser");
-require("./config/DB");
-
-const port = process.env.PORT || 5000;
+const connectDB = require("./db/connect");
 
 const app = express();
 app.use(cookieParser());
@@ -24,4 +22,16 @@ app.use("/products", productRouter);
 app.use("/orders", orderRouter);
 app.use("/dashboard", dashboardRouter);
 
-app.listen(port, () => console.log(`server is running on port ${port}`));
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    // connectDB
+    await connectDB(process.env.MONGODB_CONNECTION_URL);
+    app.listen(port, () => console.log(`Server is listening port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
