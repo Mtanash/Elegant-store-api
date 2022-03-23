1220b235824f3351031f986a176e04f605590ce1
Mohamed Tanash <tanashkoo@gmail.com>
2022-03-23 17:34:06 +0200

    Update populate.js
    add rates and review data to database population process

--- populate.js
+++ populate.js
@@ -3,11 +3,15 @@ var colors = require("colors");
 const Product = require("./models/Product");
 const User = require("./models/User");
 const Order = require("./models/Order");
+const Rate = require("./models/Rate");
+const Review = require("./models/Review");
 const connectDB = require("./db/connect");
 
 const productsData = require("./data/products.js");
 const usersData = require("./data/users.js");
 const ordersData = require("./data/orders.js");
+const ratesData = require("./data/rates.js");
+const reviewsData = require("./data/reviews.js");
 
 const start = async () => {
   try {
@@ -21,6 +25,10 @@ const start = async () => {
     console.log("Deleted old users data".blue);
     await Order.deleteMany();
     console.log("Deleted old orders data".blue);
+    await Rate.deleteMany();
+    console.log("Deleted old rates data".blue);
+    await Review.deleteMany();
+    console.log("Deleted old reviews data".blue);
 
     // create new data
     await Product.create(productsData);
@@ -29,6 +37,10 @@ const start = async () => {
     console.log("Added new users data".blue);
     await Order.create(ordersData);
     console.log("Added new orders data".blue);
+    await Rate.create(ratesData);
+    console.log("Added new rates data".blue);
+    await Review.create(reviewsData);
+    console.log("Added new reviews data".blue);
 
     console.log("Database populated successfully!".green);
     process.exit(0);
