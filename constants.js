09e8daff6c11c4dc7a94f00379a6821181998e1d
Mtanash <tanashkoo@gmail.com>
2022-03-30 20:57:22 +0200

    add constants file add categories constants

---
+++ constants.js
@@ -0,0 +1,21 @@
+const categories = [
+  "Women Clothes",
+  "Men Clothes",
+  "Electronics",
+  "Perfumes",
+  "Bags",
+  "Shoes",
+  "Watches",
+];
+
+const categoriesObj = {
+  womenClothes: "Women Clothes",
+  menClothes: "Men Clothes",
+  electronics: "Electronics",
+  perfumes: "Perfumes",
+  bags: "Bags",
+  shoes: "Shoes",
+  watches: "Watches",
+};
+
+module.exports = { categories, categoriesObj };
